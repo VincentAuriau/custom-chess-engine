26ff49e0d1232c491cdd82ee2e1b650e08d1ea95
VincentAuriau <auriau.vincent@gmail.com>
2021-03-20 14:51:15 +0100

    ADD: failing castling function

--- engine_test.py
+++ engine_test.py
@@ -3,7 +3,7 @@ import importlib
 importlib.reload(engine)
 
 
-def test_castling():
+def test_working_castling():
     game = engine.Game()
     game.move_from_coordinates(game.player1, 1, 4, 3, 4)
     game.move_from_coordinates(game.player2, 6, 4, 4, 4)
@@ -23,6 +23,27 @@ def test_castling():
     # big castling move
     game.move_from_coordinates(game.player2, 7, 4, 7, 2)
 
+def test_failing_castling():
+    game = engine.Game()
+    game.move_from_coordinates(game.player1, 1, 4, 3, 4)
+    game.move_from_coordinates(game.player2, 6, 4, 4, 4)
+    game.move_from_coordinates(game.player1, 0, 5, 3, 2)
+    game.move_from_coordinates(game.player2, 7, 3, 4, 6)
+    game.move_from_coordinates(game.player1, 0, 6, 2, 5)
+    game.move_from_coordinates(game.player2, 7, 1, 5, 2)
+
+    # Rook movement
+    game.move_from_coordinates(game.player1, 0, 7, 0, 6)
+    game.move_from_coordinates(game.player2, 4, 6, 4, 7)
+    game.move_from_coordinates(game.player1, 0, 6, 0, 7)
+    game.move_from_coordinates(game.player2, 4, 7, 4, 6)
+
+    # small castling move
+    game.move_from_coordinates(game.player1, 0, 4, 0, 6)
+
+
+
 
 if __name__ == '__main__':
-    test_castling()
+    test_working_castling()
+    test_failing_castling()
