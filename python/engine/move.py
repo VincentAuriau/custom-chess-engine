ec2c1dd7f10aa6b499ff072a8a00ec1a57ed6ca0
VincentAURIAU <auriau.vincent@gmail.com>
2023-10-25 23:10:03 +0200

    ADD: Move doc

--- python/engine/move.py
+++ python/engine/move.py
@@ -54,14 +54,12 @@ class Move:
         """
         self.player = player
         self.board = board
-        self.start = start
-        self.end = end
+        self.start = start # Should we take only starting coordinates ?
+        self.end = end # Should we take only landing coordinates ?
         self.extras = extras
 
-        self.moved_piece = start.get_piece()
-        if self.moved_piece is None:
-            ###print("Empty cell selected as start of a move")
-            pass
+        self.moved_piece = start.get_piece() # Check that moved_piece is not None ?
+
         self.killed_piece = self.end.get_piece()
         self.is_castling = False
         self.complementary_castling = None
@@ -76,7 +74,7 @@ class Move:
             Exact copy of self.
 
         """
-        # Rethink what needs to be copied and what needs not
+        # Rethink what needs to be copied and what needs not.
         copied_board = self.board.deepcopy()
         copied_move = Move(
             self.player,
@@ -90,21 +88,45 @@ class Move:
         return copied_move
 
     def _set_moved_attribute(self):
-        if hasattr(self.moved_piece, "has_moved"):
+        """Method to set the 'has_moved' attributes for the pieces that need to check whether 
+        they have already moved or not.
+        Also, if the piece is a Pawn, changes the last_move_is_double attribute so that it can
+        be taken 'En Passant' if this is the case.
+        """
+        if hasattr(self.moved_piece, "has_moved"): # Check if the moved piece has the attribute
             self.moved_piece.has_moved = True
-            ###print('PIECE', self.moved_piece.is_white(), self.moved_piece, "set to moved")
-            ###print(self.start.x, self.start.y, self.end.x, self.end.y)
-        if hasattr(self.moved_piece, "last_move_is_double"):
-            if abs(self.start.get_x() - self.end.get_x()) > 1:
+
+        if hasattr(self.moved_piece, "last_move_is_double"): # Check if it is a Pawn
+            if abs(self.start.get_x() - self.end.get_x()) > 1: # If the move is a double forward
                 self.moved_piece.last_move_is_double = True
             else:
                 self.moved_piece.last_move_is_double = False
 
     def _set_castling_done(self):
+        """
+        If self is a castling move, then when it is done this function sets the castling_done attribute
+        of the concerned King to True, so that it cannot use castling twice.
+        """
         assert isinstance(self.moved_piece, material.King)
         self.moved_piece.castling_done = True
 
     def _is_castling(self):
+        """
+        Checks if the current move is castling. In particular verifies that:
+            - the moved piece is a King
+            - the moved King has not already used its castling
+            - the King has not already moved
+            - the comparnion Rook is on the right Cell
+            - the companion Rook has not already moved
+            - the Cells between the King and the Rook are empty
+            - the King landing Cell is not threatened by adversary Pieces
+        Also if it is a castling move sets in self.extra the Rook that needs to be moved along the King
+
+        Returns
+        -------
+        bool
+            Whether self is castling or not.
+        """
         if not isinstance(self.moved_piece, material.King):
             ###print("not castling becasuse king not moved")
             return False
@@ -161,6 +183,8 @@ class Move:
                 ###print('king did not move to a castling position')
                 return False
 
+            # Verifying that the cells between rook and king are empty and that starting
+            # and landing cells for the king are not threatened.
             empty_cells_check = True
             not_threatened_cells = True
             for cll in must_be_empty_cells:
@@ -171,9 +195,19 @@ class Move:
                     not_threatened_cells = False
                     ###print("CELL THREATENED: ", cll.get_x(), cll.get_y())
 
+            # Verifies that both conditions are met
             conditions_to_castling = [empty_cells_check, not_threatened_cells]
             if all(conditions_to_castling):
-                self.complementary_castling = (
+                self.complementary_castling = ( # To store in self.extras :)
+                    rook_to_move,
+                    self.board.get_cell(
+                        rook_starting_coordinates[0], rook_starting_coordinates[1]
+                    ),
+                    self.board.get_cell(
+                        rook_ending_coordinates[0], rook_ending_coordinates[1]
+                    ),
+                )
+                self.extras["complementary_castling"] = (
                     rook_to_move,
                     self.board.get_cell(
                         rook_starting_coordinates[0], rook_starting_coordinates[1]
@@ -192,16 +226,33 @@ class Move:
                 return False
 
     def _is_en_passant(self):
-        if isinstance(self.moved_piece, material.Pawn):
+        """
+        Checks if the current move is an 'En passant' capture. In particular verifies that:
+            - the moved piece is a Pawn
+            - the movement of the pices is in diagonal
+            - the move crosses another Pawn
+            - this crossed Pawn is of different color
+            - this crossed Pawn last move is a double forward advance
+        Also if it is an 'En passant' capture sets the crossed Pawn as killed_piece
+        Returns
+        -------
+        bool
+            Whether self is an 'En passant' capture or not.
+        """
+        if isinstance(self.moved_piece, material.Pawn): # Only a Pawn can take 'En Passant'
             dx = self.start.get_x() - self.end.get_x()
             dy = self.start.get_y() - self.end.get_y()
-            if dy == 0 or self.killed_piece is not None:
+            # Needs the movement to be in diagonal and that no piece is on the landing Cell
+            if dy == 0 or self.killed_piece is not None: 
                 return False
 
             else:
+                # Retrieving crossed Piece
                 crossed_cell = self.board.get_cell(self.start.get_x(), self.end.get_y())
                 crossed_piece = crossed_cell.get_piece()
+                # Verifying the crossed Piece is a Pawn
                 if isinstance(crossed_piece, material.Pawn):
+                    # Verifying color and last move of crossed_piece
                     if (
                         crossed_piece.last_move_is_double
                         and crossed_piece.is_white() != self.moved_piece.is_white()
@@ -218,50 +269,89 @@ class Move:
             return False
 
     def _is_pawn_promotion(self):
+        """
+        Checks if the current move is should ends up with a Pawn promotion, meaning that:
+            - the moved piece is a Pawn
+            - the Pawn reaches the other side of the board according to its color
+        If the Piece it should be promote into is not specified in self.extras then sets Queen as promotion.
+
+        Returns
+        -------
+        bool
+            Whether self should ends up by a Pawn promotion or not.
+        """
+        # Checks the piece
         if not isinstance(self.moved_piece, material.Pawn):
             return False
         else:
-            if self.end.get_x() == 7 and self.moved_piece.is_white():
+            # Checks if the Pawn has reached the other side of the board.
+            if self.end.get_x() == 7 and self.moved_piece.is_white(): # White Piece
+                # Standard is to promote into a Queen if not specified
                 self.promote_into = self.extras.get("promote_into", "queen")
-                print(self.extras)
                 return True
-            elif self.end.get_x() == 0 and not self.moved_piece.is_white():
+
+            elif self.end.get_x() == 0 and not self.moved_piece.is_white():# Black Piece
+                # Standard is to promote into a Queen if not specified
                 self.promote_into = self.extras.get("promote_into", "queen")
                 return True
             else:
                 return False
 
     def _promote_pawn(self):
+        """
+        Organizes the Pawn promotion.
+        """
         coordinates = (self.end.get_x(), self.end.get_y())
-        print("promote into", self.promote_into)
         self.board.promote_pawn(coordinates=coordinates, promote_into=self.promote_into)
 
     def move_pieces(self):
         """
         Effectively moves pieces on board
         """
+        # Do everything from coordinates so that only board needs to be copied in self.deepcopy() ?
 
+        # Kills Piece on landing Cell if needed.
         if self.killed_piece is not None:
             self.board.kill_piece_from_coordinates((self.end.x, self.end.y))
+        # Moves Piece on the Board
         self.board.move_piece_from_coordinates(
             (self.start.x, self.start.y), (self.end.x, self.end.y)
         )
-        # ADD CASTLING
 
+        # Executes castling if needed
         if self.complementary_castling is not None and self.is_castling:
             castling_rook, rook_start, rook_end = self.complementary_castling
             self.board.move_piece_from_coordinates(
                 (rook_start.x, rook_start.y), (rook_end.x, rook_end.y)
             )
-            ###print("CASTLING DETECTED PPPPPPPPP")
+
+            # Sets castling to done
             self._set_castling_done()
 
+        # Promotes Pawn if needed
         if self._is_pawn_promotion():
             self._promote_pawn()
+
+        # Sets the different movement related attributes of Pieces
         self._set_moved_attribute()
 
-    def is_possible_move(self):  # REFONDRE
-        # To be implemented
+    def is_possible_move(self):  
+        # REFONDRE, particulièrement, faire en sorte qu'on ne vérifie chaque condition qu'une seule fois
+        # Why castling is checked here ?
+        """
+        Checks if move is possible. In particular checks that:
+            - Landing Cell is different than current Cell
+            - Checks the color of the potential Piece on the landing Cell
+            - Movement on the Board is authorized according to the Piece type
+            - If moved Piece is a King, does not land on a threatened Cell
+
+        If the Piece it should be promote into is not specified in self.extras then sets Queen as promotion.
+
+        Returns
+        -------
+        bool
+            Whether the move is legal or not.
+        """
         # Should be kept ?
 
         dx = self.start.get_x() - self.end.get_x()
@@ -282,22 +372,18 @@ class Move:
         # check color of receiving piece
         if self.killed_piece is not None:
             if self.moved_piece.is_white() == self.killed_piece.is_white():
-                ###print("Move is not legal, trying to move piece toward a cell with piece of same color")
                 return False
             if self.killed_piece.is_white() == self.player.is_white_side():
-                ###print('Player cannot take his own material')
                 return False
-
-        # CHECK NOT THREATENED CELL IF KING
 
         # good moving for type of piece selected
         is_legal_move = self.moved_piece.can_move(self.board, self)
+
+        # Why here ?
         self.is_castling = self._is_castling()
 
         if not is_legal_move:
             if not self._is_en_passant() or not self.is_castling:
-                ###print('Move is not legal, %s authorized movements not allowing it to go on this cell (%i, %i) from cell (%i, %i)' %
-                ### (self.moved_piece.__str__(), self.end.get_x(), self.end.get_y(), self.start.get_x(), self.start.get_y()))
                 return False
 
         """
@@ -350,21 +436,29 @@ class Move:
 
             return future_board.get_cell(king.x, king.y).is_threatened(future_board, king.is_white())
         """
+        # Checks if the player's King is threatened after the move.
         is_king_threatened_in_future = self._work_future_to_check_chess()
         if is_king_threatened_in_future:
-            ###print('King will be threatened / checked if this move is operated')
             return False
 
         return True
 
-    def _work_future_to_check_chess(self):
-        """TO BE DONE LAST"""
+    def _work_future_to_check_chess(self): # Can it be done better ?
+        """
+        Effectively move the Piece and check if the King is then threatened. In this case, move cannot happen.
 
-        # move = copy.deepcopy(self)
+        Returns
+        -------
+        bool
+            Whether the player's King is threatened once the move done.
+        """
+
+        # Deep Copy everything
         move = self.deepcopy()
-        # move = pickle.loads(pickle.dumps(self, -1))
+        # Move Piece
         move.move_pieces()
 
+        # Select player's King and check if threatened.
         if move.player.is_white_side():
             king = move.board.white_king
         else:
