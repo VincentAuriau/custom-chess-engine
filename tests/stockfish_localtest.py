a5a7ff6adfa18366bbaee2277fc4ba041e5f0318
VincentAURIAU <auriau.vincent@gmail.com>
2023-11-13 23:10:53 +0100

    ADD: local stockfish test example

---
+++ tests/stockfish_localtest.py
@@ -0,0 +1,21 @@
+if __name__=="__main__":
+	import sys
+	sys.path.append("../pyalapin")
+
+	import os
+	print(os.listdir())
+	print(os.listdir("pyalapin"))
+
+	from pyalapin.player.stockfish_player import StockfishPlayer
+	from pyalapin.engine import ChessGame
+
+
+	sfp = StockfishPlayer("/Users/vincent.auriau/Python/stockfish/bin/stockfish", white_side=True)
+	game = ChessGame()
+	co1, co2 = sfp.get_move_from_fen(game.to_fen())
+	game.board.draw()
+
+	game.move_from_coordinates(game.player1, *co1, *co2)
+	game.board.draw()
+
+	# sfp.quit()
